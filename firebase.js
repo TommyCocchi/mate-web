483b9567bba664371bb43c895e18ba7f02aabc90
TommyCocchi <111305584+TommyCocchi@users.noreply.github.com>
2026-02-02 12:16:27 +0100

    initial

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "INSERISCI",
+  authDomain: "INSERISCI",
+  projectId: "INSERISCI",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
